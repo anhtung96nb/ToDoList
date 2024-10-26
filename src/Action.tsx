30d9e7ebc036441f490c3f3d4ac76835c8066980
NITECO\tung.dam <Niteco@2024>
2024-10-26 22:15:33 +0700

    Create Todo test

---
+++ src/Action.tsx
@@ -0,0 +1,55 @@
+import { State } from "./model/index";
+
+type ActionBarProps = {
+  itemLeft: number;
+  state: State;
+  unmark?: boolean;
+  onStateChanged: (state: State) => void;
+  onClearCompleted: () => void;
+  onMarkDone: () => void;
+};
+
+export function ActionBar({
+  itemLeft,
+  state,
+  unmark,
+  onStateChanged,
+  onMarkDone,
+  onClearCompleted,
+}: ActionBarProps) {
+  return (
+    <div className="action-bar">
+      <label className="item-left-panel">
+        {itemLeft} item{itemLeft > 1 ? "s" : ""} left
+      </label>
+      <button
+        onClick={() => onStateChanged("All")}
+        className={state === "All" ? "btn btn-active" : "btn"}
+      >
+        All
+      </button>
+      <button
+        onClick={() => onStateChanged("Active")}
+        className={state === "Active" ? "btn btn-active" : "btn"}
+      >
+        Active
+      </button>
+      <button
+        onClick={() => onStateChanged("Completed")}
+        className={state === "Completed" ? "btn btn-active" : "btn"}
+      >
+        Completed
+      </button>
+
+      <button className="btn" onClick={onMarkDone}>
+        {unmark ? "Unmark all" : "Mark all complete"}
+      </button>
+
+      {state !== "Active" && (
+        <button onClick={onClearCompleted} className="btn">
+          Clear completed
+        </button>
+      )}
+    </div>
+  );
+}
